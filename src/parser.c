5b07071626f01e1a4eaeea86892382efd1323e73
Aery248 <harisaminada@gmail.com>
2024-10-16 07:20:50 +0200

    fix : partie 1/2 établissement des distinctions des différents tokens et séparation priorité des opérations

--- src/parser.c
+++ src/parser.c
@@ -4,9 +4,24 @@
 void parser(token ** tokens){
     for (int i = 0; tokens[i] != NULL; i++)
     {
-        if(tokens[i]->type==SEMICOLON){
-        printf("%d ",tokens[i]->type);
-        printf("%s\n",tokens[i]->value);
+        if(tokens[i]->type==SEMICOLON){ // si dans les tokens le caractère semicolone apparait 
+        printf("%d ",tokens[i]->type); // alors afficher son typ 
+        printf("%s\n",tokens[i]->value); // et sa valeur
+        }
+        if(tokens[i]->type==NUMBER){ // même chose pour les numéros
+            printf("%d\n",tokens[i]->type); 
+            printf("%d\n",tokens[i]->value); 
+            
+        }
+        if(tokens[i]->type==OPERATOR){ // et les opérateurs
+            printf("%d\n",tokens[i]->type);
+            printf("%d\n", tokens[i]->value);
+            if (tokens[OPERATOR]=="*" || "/"){
+                printf("%d\n", tokens[OPERATOR]->value); 
+            }
+            else {
+                printf("+" || "-");
+            }
         }
     }
     
