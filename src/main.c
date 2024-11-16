b88b0d3202f12dba0c7ee4903c486dbf326d0a23
PouletDesMers <droucook@gmail.com>
2024-11-16 18:55:31 +0100

    fix : passage d'allocation statiaue à dynamique

--- src/main.c
+++ src/main.c
@@ -10,29 +10,50 @@ void file_mode(const char *filename) {
         exit(EXIT_FAILURE);
     }
 
-    char line[256];
-    while (fgets(line, sizeof(line), file)) {
-        // Enlever le saut de ligne à la fin
+    size_t size_line = 64; 
+    char *line = malloc(size_line);
+    while (fgets(line, size_line, file)) {
+        while (strchr(line, '\n') == NULL && !feof(file)) {
+            size_line *= 2;
+            char *new_line = realloc(line, size_line);
+            line = new_line;
+        }
+
         line[strcspn(line, "\n")] = '\0';
+
         interpret(line);
     }
 
+    free(line);
     fclose(file);
 }
 
 int main(int argc, char **argv) {
     if (argc == 1) {
         // Mode interactif avec REPL (Read-Eval-Print Loop)
-        char input[256];
         printf("Mode interactif. Tapez 'exit' pour quitter.\n");
+        size_t size_input = 64; 
+        char *input = malloc(size_input);
         while (1) {
             printf("> ");
-            if (fgets(input, sizeof(input), stdin) == NULL) break; // Fin si CTRL+D
+            
+            // Lire l'entrée utilisateur
+            if (!fgets(input, size_input, stdin)) break; // Fin si CTRL+D
+
+            while (input[strlen(input) - 1] != '\n' && !feof(stdin)) {
+                size_input *= 2;
+                char *new_input = realloc(input, size_input);
+                input = new_input;
+            }
+
             // Enlever le saut de ligne à la fin
             input[strcspn(input, "\n")] = '\0';
-            if (strcmp(input, "exit") == 0) break; // Commande 'exit'
-            interpret(input); // Interpréter l'entrée
+
+            // Vérifier la commande 'exit'
+            if (strcmp(input, "exit") == 0) break;// Commande 'exit'
+            interpret(input);
         }
+        free(input);
     } else if (argc == 2) {
         // Mode fichier : lire les instructions depuis le fichier fourni
         file_mode(argv[1]);
